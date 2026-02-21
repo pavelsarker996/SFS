1a15a1ebab5f93125b02b1c89e3a7da475d82066
pavelsarker996 <sultanularefinpavel@gmail.com>
2026-02-21 17:31:35 +0600

    All Files

---
+++ demo(user).php
@@ -0,0 +1,26 @@
+
+<?php
+
+include "demo_header.php"
+
+?>
+
+
+
+        <div class="row" style="margin: 0px; padding:0px; margin-bottom: 50px;">
+
+            <div class="col-lg-6 col-lg-push-3" style="min-height: 500px; background-color: white;">
+                fiwhwisehfouqwe
+        
+            </div>
+
+        </div>
+
+
+
+        <?php
+
+include "demo_footer.php"
+
+?>
+
